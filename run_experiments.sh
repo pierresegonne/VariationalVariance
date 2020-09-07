753d477c03ea4afe32a08fed559a131b35a02118
Andrew Stirn <andrew.stirn@gmail.com>
2020-09-07 09:36:37 -0400

    non-parallel processing option

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,8 +3,7 @@
 # download data
 #python regression_data.py
 
-# set number of parallel jobs
-N=4
+
 
 # set mode
 MODE="resume"
@@ -21,6 +20,9 @@ MODE="resume"
 #  done
 #done
 
+# number of parallel jobs
+N=4
+
 # run UCI regression experiments
 declare -a Datasets=("boston" "carbon" "concrete" "energy" "naval" "power plant" "superconductivity" "wine-red" "wine-white" "yacht")
 declare -a Algorithms=("Gamma-Normal" "LogNormal-Normal")
@@ -29,13 +31,19 @@ for data in  "${Datasets[@]}"; do
   for alg in "${Algorithms[@]}"; do
     for prior in "${PriorTypes[@]}"; do
 
-      # run job
-      python regression_experiments_v2.py --dataset $data --algorithm $alg --prior_type $prior --mode $MODE \
-        --batch_iterations 200000 --k 100 --parallel 1 &
+      if [ $N -gt 1 ]; then
+        # run jobs in parallel
+        python regression_experiments_v2.py --dataset "$data" --algorithm $alg --prior_type $prior --mode $MODE \
+          --batch_iterations 200000 --k 100 --parallel 1 &
 
-      # allow N jobs in parallel
-      if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
-        wait -n
+        # allow N jobs in parallel
+        if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+          wait -n
+        fi
+      else
+        # run single job
+        python regression_experiments_v2.py --dataset "$data" --algorithm $alg --prior_type $prior --mode $MODE \
+          --batch_iterations 200000 --k 100 --parallel 0
       fi
     done
   done
